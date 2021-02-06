22a3953534e099480f047c7ee4c0af2a7f535c01
fabio <fabioz@uninorte.edu.co>
2021-02-06 12:05:35 -0500

    Process param id, update and delete for Tasks

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -22,23 +22,52 @@ exports.all = async (req, res, next) => {
   }
 };
 
-exports.read = (req, res, next) => {
-  const { params = {} } = req;
-  const { id } = params;
-  res.json({ id });
+exports.id = async (req, res, next, id) => {
+  try {
+    const doc = await Model.findById(id).exec();
+    if (!doc) {
+      const message = `${Model.modelName} not found`;
+
+      next({
+        message,
+        statusCode: 404,
+        level: 'warn',
+      });
+    } else {
+      req.doc = doc;
+      next();
+    }
+  } catch (error) {
+    next(new Error(error));
+  }
 };
 
-exports.update = (req, res, next) => {
-  const { body = {}, params = {} } = req;
-  const { id } = params;
-  res.json({
-    id,
-    body,
-  });
+exports.read = async (req, res, next) => {
+  const { doc = {} } = req;
+
+  res.json(doc);
 };
 
-exports.delete = (req, res, next) => {
-  const { params = {} } = req;
-  const { id } = params;
-  res.json({ id });
+exports.update = async (req, res, next) => {
+  const { doc = {}, body = {} } = req;
+
+  Object.assign(doc, body);
+
+  try {
+    const updated = await doc.save();
+    res.json(updated);
+  } catch (error) {
+    next(new Error(error));
+  }
+};
+
+exports.delete = async (req, res, next) => {
+  const { doc = {} } = req;
+
+  try {
+    const removed = await doc.remove();
+    res.json(removed);
+  } catch (error) {
+    next(new Error(error));
+  }
 };
--- server/api/v1/tasks/routes.js
+++ server/api/v1/tasks/routes.js
@@ -10,6 +10,8 @@ const controller = require('./controller');
 
 router.route('/').post(controller.create).get(controller.all);
 
+router.param('id', controller.id);
+
 router
   .route('/:id')
   .get(controller.read)
