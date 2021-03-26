29f3123726d57e1a6ee048d13627c7bffe0e0f0d
Fabio Zapata <fabioz@uninorte.edu.co>
2021-03-25 22:29:06 -0500

    Add name and remove password fields for Users

--- server/api/v1/users/model.js
+++ server/api/v1/users/model.js
@@ -32,7 +32,37 @@ const fields = {
 
 const user = new Schema(fields, {
   timestamps: true,
+  toJSON: {
+    virtuals: true,
+  },
+  toObject: {
+    virtuals: true,
+  }
 });
+
+
+user
+  .virtual('name').get(function getName() {
+    return `${this.firstName} ${this.lastName}`;
+  })
+  .set(function setName(name) {
+    const [firstName = '', lastName = ''] = name.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
+  });
+
+const hiddenFields = ['password'];
+
+user.methods.toJSON = function toJSON() {
+  const doc = this.toObject();
+  hiddenFields.forEach((field) => {
+    if (Object.hasOwnProperty.call(doc, field)) {
+      delete doc[field];
+    }
+  });
+  return doc;
+};
+
 
 module.exports = {
   Model: mongoose.model('user', user),
